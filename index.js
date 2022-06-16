e2be6d59e0da2a3cd555d90a91b0c38feca685d0
Otieno-derrick <otieno.derrick@student.moringaschool.com>
2022-06-16 21:42:56 +0300

    complete

---
+++ index.js
@@ -0,0 +1,9 @@
+//  const divide = (num1, num2) => 2000 / 100
+
+function divide(num1, num2) {
+    return 2000 / 100
+}
+
+const square = multply => multply * multply
+
+const add = (number1, number2) => number1 + number2
